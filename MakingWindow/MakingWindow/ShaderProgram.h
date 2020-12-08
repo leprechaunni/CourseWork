751ba9b37c2714630bb1b4882a52492abdc7c261
leprechaunni <73756514+leprechaunni@users.noreply.github.com>
2020-12-08 19:41:53 +0300

    shaders class

---
+++ MakingWindow/MakingWindow/ShaderProgram.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <string>
+
+namespace RenderEngine {
+    class ShaderProgram {
+    public:
+        ShaderProgram(const std::string& vertexShader, const std::string& fragmentShader);
+        ~ShaderProgram();
+        bool isCompiled() const { return m_isCompiled; }
+        void use() const;
+        void setInt(const std::string& name, const GLint value);
+        void setFloat(const std::string& name, const GLfloat value);
+
+        ShaderProgram() = delete;
+        ShaderProgram(const ShaderProgram&) = delete;
+        ShaderProgram& operator=(const ShaderProgram&) = delete;
+        ShaderProgram& operator=(ShaderProgram&& shaderProgram) noexcept;
+        ShaderProgram(ShaderProgram&& shaderProgram) noexcept;
+        GLuint m_ID = 0;
+
+    private:
+        bool createShader(const std::string& source, const GLenum shaderType, GLuint& shaderID);
+
+        bool m_isCompiled = false;
+    };
+}
